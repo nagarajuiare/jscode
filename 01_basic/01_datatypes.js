3fa01d5080ad5348cb79c8f39fa10b8c073b679b
Nagaraju_Gatla <128966417+nagarajuiare@users.noreply.github.com>
2025-04-06 11:30:05 +0000

    saved

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,8 @@
+let score = undefined
+
+console.log(typeof(score));
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
---
+++ 01_basic/03_operations.js
@@ -0,0 +1,6 @@
+let val = 4
+let negval = -val
+
+console.log(negval)
+console.log(typeof "1" + 2);
+console.log(typeof 1 + "2");
